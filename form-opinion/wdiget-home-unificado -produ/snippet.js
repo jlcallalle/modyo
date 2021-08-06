b2037c8cb2a44f96b2c85a6d5fda28b57540f925
Jorge Callalle <jlcallalle@gmail.com>
2021-08-06 17:21:45 -0500

    actualiza form

---
+++ form-opinion/wdiget-home-unificado -produ/snippet.js
@@ -0,0 +1,10 @@
+<div id="modyo-917ef33b-fcc3-4b86-aba0-1e035ec3c098"></div>
+<script>
+  (function(){
+    var script = document.createElement("script");
+    script.src = "https://portal-corredores.consorcio.cl/widgets/modyo_form.js?form=917ef33b-fcc3-4b86-aba0-1e035ec3c098&site=95c77c99-6aae-424d-b5fd-67d80d0045e4";
+    script.async = true;
+    var entry = document.getElementsByTagName("script")[0];
+    entry.parentNode.insertBefore(script, entry);
+  })();
+</script>
