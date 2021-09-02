ec35a609ac0f34c1f4b6da23ac99157dac906e73
Jorge Callalle <jlcallalle@gmail.com>
2021-09-02 16:08:59 -0500

    an updated commit message

---
+++ formulario-tabla/js/main.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+    console.log('tes');
+
+    $('.contenido-dinamico').hide();
+
+    $( ".box-single-video" ).click(function() {
+        var titulo = $(this).attr('title');
+        var parrafo = $(this).find('.texto-video').text();
+        var urVideo = $(this).find('.link-video').text();
+        var imgVideo = $(this).find('.imagen-video').attr('src');
+        var srcPrincipal = $("#videoclip").attr('src');
+        var posterPrincipal = $("#videoclip").attr('poster');
+
+        $("#titulo-principal").text(titulo);
+        $("#texto-principal").text(parrafo);
+        $("#videoclip").attr('src', urVideo);
+        $("#videoclip").attr('poster', imgVideo);
+
+        console.log('titulo', titulo);
+        console.log('parrafo', parrafo);
+        console.log('urVideo', urVideo);
+        console.log('srcPrincipal', srcPrincipal);
+        console.log('imgVideo', imgVideo);
+        console.log('posterPrincipal', posterPrincipal);
+    });
+
+});
+
