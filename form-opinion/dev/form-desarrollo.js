8f57f316a9bba84a531cac2b6317acd7414f96e0
Jorge Callalle <jlcallalle@gmail.com>
2021-09-09 15:25:53 -0500

    add new questions form modyo

--- form-opinion/dev/form-desarrollo.js
+++ form-opinion/dev/form-desarrollo.js
@@ -7,11 +7,13 @@ $("#answers_13_value").attr('maxlength','70');
 $("#answers_12_value").attr("placeholder", "Ingrese aquí tu comentario");
 $("#answers_12_value").attr('maxlength','140');
 
-$(".radio").wrapAll( "<div class='box-radio' />");
-
+var pregunta = "Basado en tu experiencia en el uso de la Plataforma Corredores, en una escala del 0 al 10, en donde 0 es “Muy improbable” y 10 “Muy probable”, ";
 $(".users-form-response-form .form-group").each(function(index, value){
-	if ( index== "3" ) {
+	console.log('index',index);
+	  if ( index== "3" || index== "4") {
       $( this ).addClass("ranking");
+			$( this ).find('small').nextAll().wrapAll( "<div class='box-radio' />");
+			$( this ).find('small').prepend( "<span>"+pregunta+"</span>" );
     } 
 });
 
